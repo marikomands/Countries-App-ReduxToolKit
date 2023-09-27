19bf0cf81fcf7875cadede9ef76967affb6b6194
marikomands <marikomands@gmail.com>
2023-09-27 14:49:57 +0100

    Created App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import CountryDetails from "./components/countryDetails/CountryDetails";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route index element={<Home />} />
+          <Route path="/:code" element={<CountryDetails />} />
+        </Route>
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
