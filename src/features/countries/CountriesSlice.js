02c82c43334c0806dd52e96d1e47f3c3aee31e5c
marikomands <marikomands@gmail.com>
2023-09-27 14:49:19 +0100

    Created Slice

---
+++ src/features/countries/CountriesSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const countriesSlice = createSlice({
+  name: "countries",
+  initialState: {},
+  reducers: {},
+});
+
+export default countriesSlice.reducer;
