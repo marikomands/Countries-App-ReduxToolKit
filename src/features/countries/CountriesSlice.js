d2d231e106cdd7cc6c07a2f3b50b7e4b98ae0942
marikomands <marikomands@gmail.com>
2023-09-29 14:34:27 +0100

    created countriesSlice

--- src/features/countries/CountriesSlice.js
+++ src/features/countries/CountriesSlice.js
@@ -1,9 +1,36 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { showAllCountries } from "./CountriesAction";
 
-const countriesSlice = createSlice({
+const initialState = {
+  loading: false,
+  countriesData: [],
+  countryData: [],
+  success: false,
+  error: false,
+  message: "",
+};
+
+const CountriesSlice = createSlice({
   name: "countries",
-  initialState: {},
+  initialState: initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(showAllCountries.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(showAllCountries.fulfilled, (state, action) => {
+        state.loading = false;
+        state.countriesData = action.payload;
+        state.success = true;
+      })
+      .addCase(showAllCountries.rejected, (state, action) => {
+        state.loading = false;
+        state.error = true;
+        state.message = action.payload;
+        state.counrtiesData = [];
+      });
+  },
 });
 
-export default countriesSlice.reducer;
+export default CountriesSlice.reducer;
