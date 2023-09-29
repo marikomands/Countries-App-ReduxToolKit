e60afb100fa5b2bb8e7d0b39fba32fac4b0935ab
marikomands <marikomands@gmail.com>
2023-09-29 14:36:26 +0100

    minor change

--- src/components/countryDetails/CountryDetails.js
+++ src/components/countryDetails/CountryDetails.js
@@ -1,5 +1,7 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { showAllCountries } from "../../features/countries/CountriesAction";
 
 export default function CountryDetails() {
-  return <div>Country Details</div>;
+  return <div>hi</div>;
 }
