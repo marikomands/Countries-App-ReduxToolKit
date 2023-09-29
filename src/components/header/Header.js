1923ff20785dcfe4326ae6de1cebdd6c86ab059f
marikomands <marikomands@gmail.com>
2023-09-29 14:35:36 +0100

    Created Header component

---
+++ src/components/header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Country from "../country/Country";
+import "./Header.css";
+
+const header = () => {
+  return (
+    <div>
+      <h1 className="header">Where In The World?</h1>
+      <Outlet />
+    </div>
+  );
+};
+
+export default header;
