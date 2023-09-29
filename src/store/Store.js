885daf4ce632d64dbbd07bfab96e420cab722975
marikomands <marikomands@gmail.com>
2023-09-29 14:34:14 +0100

    changed reducer name

--- src/store/Store.js
+++ src/store/Store.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import countriesReducer from "../features/countries/CountriesSlice";
 
 const store = configureStore({
-  reducer: { country: countriesReducer },
+  reducer: { countries: countriesReducer },
 });
 
 export default store;
