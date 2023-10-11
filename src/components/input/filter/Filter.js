cfd725b238e07e6bdf8a9f8ddef503f5a4503913
marikomands <marikomands@gmail.com>
2023-10-11 14:25:45 +0100

    Filter updated with regions and using React Select

--- src/components/input/filter/Filter.js
+++ src/components/input/filter/Filter.js
@@ -1,33 +1,88 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import Select from "react-select";
+import { reset, setRegion } from "../../../features/countries/CountriesSlice";
 import "./Filter.css";
 
 const Filter = () => {
-  // const [countryList, setCountryList] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState();
+  const regions = [
+    { value: "africa", label: "Africa" },
+    { value: "america", label: "America" },
+    { value: "asia", label: "Asia" },
+    { value: "oceania", label: "Oceania" },
+  ];
 
-  const countries = [{ name: "Japan" }, { name: "UK " }, { name: "China" }];
+  const [selectedOption, setSelectedOption] = useState(null);
 
-  const handleCategoryChange = (event) => {
-    setSelectedCategory(event.target.value);
-  };
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (selectedOption) {
+      dispatch(setRegion(selectedOption));
+    }
+
+    return () => {
+      dispatch(reset());
+    };
+  }, [dispatch, selectedOption]);
+
+  // const [filter, setFilter] = useState("");
+  // const [displayDropdown, setDisplayDropdown] = useState(false);
+
+  // const handleDropdownChange = () => {
+  //   setDisplayDropdown(!displayDropdown);
+  // };
 
   return (
-    <div className="filter-container">
-      <div>Filter by Category:</div>
-      <div>
-        <select
-          className="category-list"
-          id="category-list"
-          onChange={handleCategoryChange}
-        >
-          <option value="">All</option>
-          <option value="Japan">Japan</option>
-          <option value="UK">UK</option>
-          <option value="China">China</option>
-        </select>
-      </div>
-      {selectedCategory}
+    <div className="select-container">
+      <Select
+        className="select"
+        defaultValue={selectedOption}
+        onChange={(option) => {
+          setSelectedOption(option.value);
+        }}
+        options={regions}
+        placeholder="Select by Region"
+        styles={{
+          control: (provided, state) => ({
+            ...provided,
+            borderColor: state.isFocused
+              ? "grey !important"
+              : "grey !important",
+            boxShadow: state.isFocused ? "none !important" : null,
+            // height: "100%",
+          }),
+        }}
+      />
     </div>
+
+    // <section>
+    //   <div className="filter-container" onClick={handleDropdownChange}>
+    //     { <input
+    //       className="filter-input"
+    //       type="text"
+    //       value={filter}
+    //       placeholder="Search by Region"
+    //     />
+    //     <i className="fa-solid fa-angle-down"></i> }
+    //     {displayDropdown ? (
+    //       <div className="drop-down">
+    //         {regions.map((region) => {
+    //           return (
+    //             <div
+    //               onClick={() => {
+    //                 setFilter(region);
+    //                 handleDropdownChange();
+    //               }}
+    //             >
+    //               {region}
+    //             </div>
+    //           );
+    //         })}
+    //       </div>
+    //     ) : null}
+    //   </div>
+    // </section>
   );
 };
 
